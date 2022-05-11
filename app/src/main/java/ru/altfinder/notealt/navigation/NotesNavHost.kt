1b6f849aa1116ce3f675b5876bceba085b3b359c
Kirill Anikin <baran.kursk@gmail.com>
2022-05-11 21:30:43 +0300

    Update-2

--- app/src/main/java/ru/altfinder/notealt/navigation/NotesNavHost.kt
+++ app/src/main/java/ru/altfinder/notealt/navigation/NotesNavHost.kt
@@ -4,10 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import ru.altfinder.notealt.screens.Add
-import ru.altfinder.notealt.screens.Main
-import ru.altfinder.notealt.screens.Note
-import ru.altfinder.notealt.screens.Start
+import ru.altfinder.notealt.screens.*
 
 sealed class NavRoute(val route: String) {
     object  Start: NavRoute(route ="start_screen" )
@@ -21,10 +18,10 @@ fun NotesNavHost() {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = NavRoute.Start.route) {
-        composable(NavRoute.Start.route) { Start(navController = navController ) }
-        composable(NavRoute.Main.route) { Main(navController = navController ) }
-        composable(NavRoute.Add.route) { Add(navController = navController ) }
-        composable(NavRoute.Note.route) { Note(navController = navController ) }
+        composable(NavRoute.Start.route) { StartScreen(navController = navController ) }
+        composable(NavRoute.Main.route) { MainScreen(navController = navController ) }
+        composable(NavRoute.Add.route) { AddScreen(navController = navController ) }
+        composable(NavRoute.Note.route) { NoteScreen(navController = navController ) }
         
 
     }
